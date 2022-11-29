7acc9c3edd929b9cbe0198a2cc285d636ba451ec
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-29 15:31:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(record){
+    const result = record.find((recordCheck) => recordCheck.result==="W");
+    if (result){
+    return result.year;
+    } 
+}
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ];
+  
+
+
+
+  
